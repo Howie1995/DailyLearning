5088268423d5471b03608b34fafbfef73f268a9c
Howie1995 <18883846670@163.com>
2018-11-01 16:59:13 +0800

    Firt Commit

---
+++ AliOss/src/main/java/org/howie/AliOss/App.java
@@ -0,0 +1,13 @@
+package org.howie.AliOss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
